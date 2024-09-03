8c08f51ecc068176c33fd6c04ff072879ac390b3
gia-grigalashvili <grigalagia07@gmail.com>
2024-09-04 01:01:11 +0400

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div>
+      <h1 className="text-[24px] text-white">pomodoro</h1>
+      <div className="w-[327px] h-[60px] bg-[#161932] items-center justify-center flex gap-[15px]">
+        <h1 className="w-[105px]">pomodoro</h1>
+        <h1>short break</h1>
+        <h1>long break</h1>
+      </div>
+    </div>
+  );
+}
